0d8e486f14ad154022b7f50829af5ec8e6359cbd
Victor Hugo <victorhugof.lisboa@gmail.com>
2023-12-22 03:29:52 -0300

    first commit

---
+++ io.asm
@@ -0,0 +1,63 @@
+SECTION .text
+global printstr, getstr
+
+; printstr
+; Args:
+;   pointer to string
+; Returns:
+;   -
+; Description:
+;   prints string
+printstr:
+    enter 0, 0
+    push DWORD [ebp+8]  ; push string pointer
+    call strlen
+    mov edx, eax        ; size of string
+    mov eax, 4          ; write syscall
+    mov ecx, [ebp+8]    ; pointer to string
+    mov ebx, 1          ; stdout
+    int 80h
+    leave
+    ret 4
+
+; strlen
+; Args:
+;   pointer to string
+; Returns:
+;   size of string
+; Description:
+;   returns string size it assumes the string ends with '\0'
+strlen:
+    enter 0, 0
+    mov eax, [ebp+8]
+
+strlen_loop:
+    cmp BYTE [eax], 0   ; end of string
+    je strlen_end
+    inc eax
+    jmp strlen_loop
+
+strlen_end:
+    sub eax, [ebp+8]    ; end - begin = size
+    leave
+    ret 4
+
+; getstr
+; Args:
+;   pointer to string
+;   size of string to read
+; Returns:
+;   -
+; Description:
+;   reads string from keyboard and stores it in pointer
+getstr:
+    enter 0, 0
+    
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, [ebp+12]   ; pointer to string
+    mov edx, [ebp+8]    ; size of string
+    int 80h
+
+    leave
+    ret 8
---
+++ main.asm
@@ -0,0 +1,58 @@
+SECTION .data
+
+msg1 db "Bem vindo. Digite seu nome: ", 0
+msg2 db "Hola, ", 0
+msg3 db ", bem-vindo ao programa de CALC IA-32", 10, 0
+msg4 db	"Vai trabalhar com 16 ou 32 bits (digite 0 para 16, e 1 para 32): ", 0
+menu1 db "ESCOLHA UMA OPÇÃO:", 10, 0
+menu2 db "- 1: SOMA", 10, 0
+menu3 db "- 2: SUBTRACAO", 10, 0
+menu4 db "- 3: MULTIPLICACAO", 10, 0
+menu5 db "- 4: DIVISAO", 10, 0
+menu6 db "- 5: EXPONENCIACAO", 10, 0
+menu7 db "- 6: MOD", 10, 0
+menu8 db "- 7: SAIR", 10, 0
+overflow db "OCORREU OVERFLOW", 10, 0
+
+SECTION .bss
+
+name resb 60
+precision resb 2
+menuOpt resb 2
+
+SECTION .text
+global _start
+extern printstr, getstr
+
+_start:
+
+    push msg1
+    call printstr
+    
+    ; get user's name
+    push name
+    push 60
+    call getstr
+
+    ; welcome to user
+    push msg2
+    call printstr
+    
+    push name
+    call printstr
+
+    push msg3
+    call printstr
+
+    ; asks for precision
+    push msg4
+    call printstr
+
+    push precision
+    push 2
+    call getstr
+
+    ; exit
+    mov eax, 1
+    mov ebx, 0
+    int 80h
